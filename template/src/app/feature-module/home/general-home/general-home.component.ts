82add31af9301a1d0139af6ac8e8f720f062a45f
harshthakkar-code <harryparkar111@gmail.com>
2025-07-10 19:04:00 +0530

    home page chnages

--- template/src/app/feature-module/home/general-home/general-home.component.ts
+++ template/src/app/feature-module/home/general-home/general-home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { routes } from 'src/app/shared/routes/routes';
+import api from 'src/app/shared/api/axios';
 
 @Component({
   selector: 'app-general-home',
@@ -10,7 +11,67 @@ import { routes } from 'src/app/shared/routes/routes';
   templateUrl: './general-home.component.html',
   styleUrl: './general-home.component.scss'
 })
-export class GeneralHomeComponent {
+export class GeneralHomeComponent implements OnInit {
+  featuredDoctors: any[] = [];
+  featuredDoctorsLimit = 6; // Set to your current API limit
+  loading = false;
+  reviews: any[] = [];
+  reviewLimit = 6;
+  specializationCounts: any[] = [];
+
+  staticSpecialties = [
+    { name: 'Cardiology', img: 'assets/img/specialities/speciality-01.jpg', icon: 'assets/img/specialities/speciality-icon-01.svg' },
+    { name: 'Orthopedics', img: 'assets/img/specialities/speciality-02.jpg', icon: 'assets/img/specialities/speciality-icon-02.svg' },
+    { name: 'Neurology', img: 'assets/img/specialities/speciality-03.jpg', icon: 'assets/img/specialities/speciality-icon-03.svg' },
+    { name: 'Pediatrics', img: 'assets/img/specialities/speciality-04.jpg', icon: 'assets/img/specialities/speciality-icon-04.svg' },
+    { name: 'Psychiatry', img: 'assets/img/specialities/speciality-05.jpg', icon: 'assets/img/specialities/speciality-icon-05.svg' },
+    { name: 'Endocrinology', img: 'assets/img/specialities/speciality-06.jpg', icon: 'assets/img/specialities/speciality-icon-06.svg' },
+    { name: 'Pulmonology', img: 'assets/img/specialities/speciality-07.jpg', icon: 'assets/img/specialities/speciality-icon-07.svg' },
+    { name: 'Urology', img: 'assets/img/specialities/speciality-08.jpg', icon: 'assets/img/specialities/speciality-icon-08.svg' }
+  ];
+
+  getSpecialtyCount(name: string): number {
+    const found = this.specializationCounts.find(s => s.name.toLowerCase() === name.toLowerCase());
+    return found ? found.count : 10;
+  }
+
+  async ngOnInit() {
+    this.loading = true;
+    try {
+      const res = await api.get('/doctor/public', {
+        params: {
+          page: 1,
+          limit: this.featuredDoctorsLimit,
+          sort: 'rating', // adjust if your backend uses a different param
+          order: 'desc'
+        }
+      });
+      this.featuredDoctors = res.data.data || [];
+    } catch (err) {
+      this.featuredDoctors = [];
+    }
+    await this.fetchReviews();
+    await this.fetchSpecializationCounts();
+    this.loading = false;
+  }
+
+  async fetchReviews() {
+    try {
+      const res = await api.get('/reviews');
+      this.reviews = (res.data.reviews || []).slice(0, this.reviewLimit);
+    } catch (err) {
+      this.reviews = [];
+    }
+  }
+
+  async fetchSpecializationCounts() {
+    try {
+      const res = await api.get('/specialization/count-by-name');
+      this.specializationCounts = res.data || [];
+    } catch (err) {
+      this.specializationCounts = [];
+    }
+  }
 public routes=routes;
 time: Date | null = null; // Bind this to the p-calendar
 bsValue=new Date();
